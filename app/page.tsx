643de42f44605fbd4f116b8395d39d42b1ab11c1
Mikael Lirbank <mikael@lirbank.com>
2025-04-13 11:47:02 -0700

    LP

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 export default function Home() {
   return (
-    <main className="flex h-dvh items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+    <main className="flex h-dvh flex-col items-center justify-center gap-10 p-5">
+      <img src="/starmode-logo.svg" alt="logo" className="w-full max-w-xl" />
+      <div className="flex flex-col gap-2">
+        <h1 className="text-center text-4xl font-bold">template-nextjs</h1>
+        <a
+          className="text-center underline hover:no-underline"
+          href="https://github.com/starmode-base/template-nextjs"
+          target="_blank"
+        >
+          https://github.com/starmode-base/template-nextjs
+        </a>
+      </div>
     </main>
   );
 }
